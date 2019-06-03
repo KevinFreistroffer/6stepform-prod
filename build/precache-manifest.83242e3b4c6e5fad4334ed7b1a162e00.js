263fff558f268d083477eb0b29901504f9f34878
Kevin <kevin.freistroffer@gmail.com>
2019-06-03 01:42:30 -0700

    build

---
+++ build/precache-manifest.83242e3b4c6e5fad4334ed7b1a162e00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8894da47e98e313eca7f3132c63d0ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20948fffadc1948e1b8b",
+    "url": "/static/css/main.49f4f220.chunk.css"
+  },
+  {
+    "revision": "20121a4ecc991433489c",
+    "url": "/static/js/2.81c056eb.chunk.js"
+  },
+  {
+    "revision": "20948fffadc1948e1b8b",
+    "url": "/static/js/main.a3afc67b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f481a27ea85aed28b98925e74396377",
+    "url": "/static/media/logo.3f481a27.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,25 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('*', (req, res, next) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+	console.log(`Listening on port ${app.get('port')}`);
+});
+
+module.exports = app;
